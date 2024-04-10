f322a4970b367777bbb0042c4aa8ebb538020c17
Sanam Gurung <83165060+Sanam-Bdr-Gurung@users.noreply.github.com>
2024-04-10 14:55:47 +0545

    Update AudioServiceActivity.java
    remove Fragment activity

--- audio_service/android/src/main/java/com/ryanheise/audioservice/AudioServiceActivity.java
+++ audio_service/android/src/main/java/com/ryanheise/audioservice/AudioServiceActivity.java
@@ -3,8 +3,6 @@ package com.ryanheise.audioservice;
 import android.content.Context;
 
 import androidx.annotation.NonNull;
-
-import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.android.FlutterFragmentActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 
