bd838eb1f8409c80c4cb5bc2efa095d887d7d2f1
Sanam Gurung <83165060+Sanam-Bdr-Gurung@users.noreply.github.com>
2024-04-10 14:43:04 +0545

    Update AudioServiceActivity.java
    fix syntax error

--- audio_service/android/src/main/java/com/ryanheise/audioservice/AudioServiceActivity.java
+++ audio_service/android/src/main/java/com/ryanheise/audioservice/AudioServiceActivity.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterFragmentActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 
 public class AudioServiceActivity extends FlutterFragmentActivity {
